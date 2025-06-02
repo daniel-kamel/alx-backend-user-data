3402913ed909a98d998b2f81ff4552ee243126b0
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-06-02 15:07:28 +0300

    Update .gitignore and implement DB class for user management

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,8 @@ class User(Base):
     '''User model for the user authentication service.'''
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(128), unique=True, nullable=False)
     hashed_password = Column(String(256), nullable=False)
     session_id = Column(String(64), nullable=True)
     reset_token = Column(String(256), nullable=True)
